364fd68b1f1b49b3b2239e1716d9823a6425a668
emjorome <emjorome@espol.edu.ec>
2023-10-30 22:17:10 -0500

    conflicto resuelto

--- Hamburger/src/Hamburger/HealthyBurger.java
+++ Hamburger/src/Hamburger/HealthyBurger.java
@@ -11,6 +11,8 @@ public class HealthyBurger extends BaseBurger {
         System.out.println("Mustard");
         additions.add(6);
         System.out.println("Ketchup");
+        additions.add(7);
+        System.out.println("Salad Cream");
 
     }
 
